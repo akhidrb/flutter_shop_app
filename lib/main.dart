53276d964eeec94cf425e14b12504f7e246a3506
Ibrahim Khidr <ibrahim.khidr@gmail.com>
2022-06-03 20:33:10 +0200

    apply state management using providers package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:udemy_shop_app/providers/products_provider.dart';
+import 'package:udemy_shop_app/screens/product_detail_screen.dart';
 import 'package:udemy_shop_app/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
@@ -6,14 +9,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My Shop",
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: "Lato",
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: "My Shop",
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: "Lato",
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:udemy_shop_app/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final String title;
-
-  ProductDetailScreen(this.title);
+  static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)?.settings.arguments as String;
+    final loadedProduct = Provider.of<ProductsProvider>(context, listen: false)
+        .findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(loadedProduct.title),
       ),
     );
   }
